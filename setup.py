00bc24d7cf66b286c8a3ed6f855bc57742096291
Anonymous <anon@whatever>
2021-05-16 09:24:09 +0300

    a

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py-multiapi",
+    name="py_multiapi",
     version="1",
     author="Itay K",
     author_email="itayki98@gmail.com",
     description="A Python Wrapper for api.itaykibotsonetwo.ml/docs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/iiiiii1wepfj/py-multiapi",
+    url="https://github.com/iiiiii1wepfj/py_multiapi",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
