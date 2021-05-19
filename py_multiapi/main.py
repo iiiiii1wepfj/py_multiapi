44bc372008ac0640444347ea31736543a57c42d9
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-19 10:40:21 +0300

    Update main.py

--- py_multiapi/main.py
+++ py_multiapi/main.py
@@ -9,12 +9,21 @@ api_url = "https://api.itayki.com"
 class multiapi:
     @staticmethod
     async def get_exec_langs():
+        """
+        Get the list of the supported languages to execute code using tio.run.
+        """
         a = (await http.get(f"{api_url}/execlangs")).json()
         await http.aclose()
         return a["langs"]
 
     @staticmethod
     async def exec_code(lang: str, code: str):
+        """
+        Execute code using tio.run.
+        parameters:
+        lang: the language to execute,
+        code: the code to execute.
+        """
         if not (lang or code):
             return "please specify the code or the language"
         a = (
@@ -30,6 +39,11 @@ class multiapi:
 
     @staticmethod
     async def ocr(url: str):
+        """
+        Get the text from the image.
+        parameters:
+        url: the url of the image.
+        """
         if not url:
             return "please specify the url"
         a = (await http.get(f"{api_url}/ocr", params=dict(url=url))).json()
@@ -43,6 +57,13 @@ class multiapi:
     async def translate(
         text: str, fromlang: Optional[str] = None, lang: Optional[str] = "en"
     ):
+        """
+        Translate the specified text.
+        parameters:
+        text: the text to translate,
+        fromlang (optional): the language code of the text to translate (if not specified, this is auto detect the text),
+        lang (optional): the language code of the translated text (the output text) (if not specified, this is translate to english).
+        """
         if not text:
             return "please specify the url"
         a = (
@@ -60,6 +81,11 @@ class multiapi:
 
     @staticmethod
     async def urban(query: str):
+        """
+        Get the results from urban dictionary api.
+        parameters:
+        query: the query to search.
+        """
         if not query:
             return "please specify the query"
         a = {
@@ -77,6 +103,13 @@ class multiapi:
         width: Optional[int] = 1280,
         height: Optional[int] = 720,
     ):
+        """
+        Returns a screenshot of the specified site.
+        parameters:
+        url: the url of the site to screenshot,
+        width (optional): the width of the screenshot (default to 1280),
+        height (optional): the height code of the screenshot (default to 720).
+        """
         if not url:
             return "please specify the url"
         a = await http.get(
@@ -88,6 +121,12 @@ class multiapi:
 
     @staticmethod
     async def random_number(min: Union[int, str] = 1, max: Union[int, str] = 100):
+        """
+        Returns a random number from the min parameter to the max parameter.
+        parameters:
+        min (optional): the minimum number (default to 1),
+        max (optional): the maximum number (default to 100).
+        """
         if not (min or max):
             return "please specify the min and max"
         a = (await http.get(f"{api_url}/random", params=dict(min=min, max=max))).json()
